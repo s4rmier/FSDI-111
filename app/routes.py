95874cabfa6382cc43eff9a69b0cddd04a7d1fae
Romnick Sarmiento <s4rmier@gmail.com>
2023-09-06 08:37:24 +0800

    first commit

---
+++ app/routes.py
@@ -0,0 +1,18 @@
+from flask import Flask  # From the flask module import the Flask class
+
+# OOP -> Object Oriented Paradigm
+
+app = Flask(__name__)           # Creates an instance of the Flask class
+# object
+
+
+# Flask decorator that maps view functions to routes
+@app.get("/")
+def about_me():                 # Our view function
+    me = {                      # Python dictionary
+        "first_name": "Romnick",
+        "last_name": "Sarmiento",
+        "hobbies": "Gaming",
+        "is_active": True
+    }
+    return me                   # return statement (returned to caller)
